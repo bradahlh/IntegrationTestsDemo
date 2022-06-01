f7149cb7b55557917ea6f295b336ece9ec988c9d
Andreas Bradahl <andreasbradahl@gmail.com>
2022-06-01 09:21:02 +0200

    new namespace format

--- OrdersAPI/Controllers/InvoiceController.cs
+++ OrdersAPI/Controllers/InvoiceController.cs
@@ -1,57 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OrderAPI.Models;
 using OrderAPI.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace OrderAPI.Controllers
+namespace OrderAPI.Controllers;
+
+[Route("api/[controller]")]
+public class InvoiceController : ControllerBase
 {
-    // TODO: Should this be its own controller, or part of the OrderController?
-    [Route("api/[controller]")]
-    public class InvoiceController : ControllerBase
+    private readonly ICustomerService _customerService;
+    private readonly IProductService _productService;
+
+    public InvoiceController(ICustomerService customerService, IProductService productService)
     {
-        private readonly ICustomerService _customerService;
-        private readonly IProductService _productService;
+        _customerService = customerService;
+        _productService = productService;
+    }
 
-        public InvoiceController(ICustomerService customerService, IProductService productService)
+    // POST api/values
+    [HttpPost]
+    public async Task<ActionResult<Invoice>> GenerateInvoice(Order invoiceOrder)
+    {
+        var invoice = new Invoice();
+        invoice.Id = Guid.NewGuid().ToString();
+
+        // Add customer data to invoice
+        var customer = await _customerService.GetCustomerAsync(invoiceOrder.CustomerId);
+
+        if (customer is null)
         {
-            _customerService = customerService;
-            _productService = productService;
+            return BadRequest();
         }
+        invoice.FullName = $"{customer.FirstName} {customer.LastName}";
+        invoice.Address = $"{customer.ZipCode} {customer.City}";
 
-        // POST api/values
-        [HttpPost]
-        public async Task<ActionResult<Invoice>> GenerateInvoice(Order invoiceOrder)
+        // Add product data to invoice
+        var product = await _productService.GetProductAsync(invoiceOrder.ProductId);
+
+        if (product is null)
         {
-            var invoice = new Invoice();
-            invoice.Id = Guid.NewGuid().ToString();
-
-            // Add customer data to invoice
-            var customer = await _customerService.GetCustomerAsync(invoiceOrder.CustomerId);
-
-            if (customer is null)
-            {
-                return BadRequest();
-            }
-            invoice.FullName = $"{customer.FirstName} {customer.LastName}";
-            invoice.Address = $"{customer.ZipCode} {customer.City}";
-
-            // Add product data to invoice
-            var product = await _productService.GetProductAsync(invoiceOrder.ProductId);
-
-            if (product is null)
-            {
-                return BadRequest();
-            }
-            invoice.ProductName = product.ProductName;
-            invoice.PaymentPrice = product.Price * invoiceOrder.Quantity;
-
-
-            // Add other data to invoice
-            invoice.PurchaseTime = invoiceOrder.PurchaseTime;
-            invoice.DueDate = invoiceOrder.PurchaseTime.AddDays(14);
-
-            return Ok(invoice);
+            return BadRequest();
         }
+        invoice.ProductName = product.ProductName;
+        invoice.PaymentPrice = product.Price * invoiceOrder.Quantity;
+
+
+        // Add other data to invoice
+        invoice.PurchaseTime = invoiceOrder.PurchaseTime;
+        invoice.DueDate = invoiceOrder.PurchaseTime.AddDays(14);
+
+        return Ok(invoice);
     }
 }
--- OrdersAPI/Services/ProductService.cs
+++ OrdersAPI/Services/ProductService.cs
@@ -1,32 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OrderAPI.Models;
 
-namespace OrderAPI.Services
+namespace OrderAPI.Services;
+
+public class ProductService : IProductService
 {
-    public class ProductService : IProductService
+    private readonly IHttpClientFactory factory;
+
+    public ProductService([FromServices] IHttpClientFactory factory)
     {
-        private readonly IHttpClientFactory factory;
+        this.factory = factory;
+    }
 
-        public ProductService([FromServices] IHttpClientFactory factory)
-        {
-            this.factory = factory;
-        }
+    public async Task<Product> GetProductAsync(int productId)
+    {
+        // Add product data to invoice
+        string productUri = $"api/product/{productId}";
 
-        public async Task<Product> GetProductAsync(int productId)
-        {
-            // Add product data to invoice
-            string productUri = $"api/product/{productId}";
+        HttpClient client = factory.CreateClient(name: "ProductAPI");
 
-            HttpClient client = factory.CreateClient(name: "ProductAPI");
+        HttpRequestMessage productRequest = new(
+            method: HttpMethod.Get, requestUri: productUri);
 
-            HttpRequestMessage productRequest = new(
-                method: HttpMethod.Get, requestUri: productUri);
+        HttpResponseMessage productResponse = await client.SendAsync(productRequest);
 
-            HttpResponseMessage productResponse = await client.SendAsync(productRequest);
+        Product? product = await productResponse.Content.ReadFromJsonAsync<Product>();
 
-            Product? product = await productResponse.Content.ReadFromJsonAsync<Product>();
-
-            return product;
-        }
+        return product;
     }
 }
